c50242dadfc9b8ed67c756f383f450b368ea4386
michal2396 <122690450+michal2396@users.noreply.github.com>
2023-12-06 19:14:30 +0200

    first_commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+import random
+from geopy.distance import geodesic
+
+app = Flask(__name__)
+CORS(app)
+
+# The initial user and goal locations
+user_location = {'lat': 0, 'lon': 0}
+goal_location = {'lat': 0, 'lon': 0}
+
+
+def generate_goal():
+    """Generate a random goal location within a 1 km radius from the user"""
+    goal_location['lat'] = user_location['lat'] + random.uniform(-0.01, 0.01)
+    goal_location['lon'] = user_location['lon'] + random.uniform(-0.01, 0.01)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', user_location=user_location, goal_location=goal_location)
+
+
+@app.route('/update_location/<float:lat>/<float:lon>')
+def update_location(lat, lon):
+    global user_location
+    user_location = {'lat': lat, 'lon': lon}
+
+    # Check if the user is within 10m of the goal using geopy's geodesic
+    distance_to_goal = geodesic((user_location['lat'], user_location['lon']),
+                                (goal_location['lat'], goal_location['lon'])).meters
+
+    if distance_to_goal < 10:
+        #generate_goal()
+        return jsonify({'message': 'Goal!'})
+
+    return jsonify({'message': ''})
+
+
+if __name__ == '__main__':
+    generate_goal()
+    app.run(debug=True)
